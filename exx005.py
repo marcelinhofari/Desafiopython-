f42b109f2dd18cbd9a070bf614e35dcef26854ec
marcelinhofari <marcelowork3@gmail.com>
2025-04-10 18:53:51 -0300

    Adicionei o def

--- exx005.py
+++ exx005.py
@@ -1,4 +1,9 @@
-print('Olá,Seja bem-vindo!')
-m=float(input('Digite quantos metros:'))
-c=(m*100)
-print(m,'metros em centimetros é:',c,'centimetros!')
+def Calcular_Metro():
+    try:
+        print('Olá,Seja bem-vindo!')
+        m=float(input('Digite quantos metros:'))
+        c=(m*100)
+        print(m,'metros em centimetros é:',c,'centimetros!')
+    except ValueError:
+        print('Erro. não foi possivel te enteder.') 
+Calcular_Metro()
