f5039a7364544fe2bfd6a8e489cff4adbcb6e745
marcelinhofari <marcelowork3@gmail.com>
2025-04-15 20:45:12 -0300

    exx009

---
+++ exx009.py
@@ -0,0 +1,8 @@
+def calor():
+    try:
+        f=int(input('DIgite a temperatura em farenheit:'))
+        c= (f-32)*5/9
+        print(f'A temperatura em celsius é {c:.2f}')
+    except ValueError:
+        print('Erro. Valor inválido.')
+calor()
