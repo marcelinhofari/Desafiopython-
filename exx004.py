62cb1f398daba49d9f550b2134598df867a07142
Marcelo Farias <157025476+marcelinhofari@users.noreply.github.com>
2025-05-26 11:24:52 -0300

    Update exx004.py
    upadte

--- exx004.py
+++ exx004.py
@@ -7,6 +7,7 @@ def calcular_media(n1,n2,n3,n4):
         resultado = (f"Que pena, você foi reprovado com media {m: .2f}")
     return resultado
 try:
+    
     n1=float(input('Digite a primeria nota:'))
     n2=float(input('Digite a segunda nota:'))
     n3=float(input('Digite a terceira nota:'))
